9cc31c8b649d68edac61f11bd3605a52d8dd00e2
Max Okorokov <maxim.okorokov@gmail.com>
2017-12-14 16:59:26 +0200

    tweaks
    image pixels per size
    OnDrawGizmos
    and first methods

--- Tranzpord/Assets/Code/Buildings/LivingHouse.cs
+++ Tranzpord/Assets/Code/Buildings/LivingHouse.cs
@@ -5,6 +5,12 @@ public class LivingHouse : MonoBehaviour {
     public int Residents { get; set; }
     public IntReference DefaultResidents;
 
+    private void Start()
+    {
+        Residents = DefaultResidents;
+        //Check for Current Upgrade Level
+    }
+
     //Current Upgrade Level
 
     //Upgrade
--- Tranzpord/Assets/Code/Stations/CityBusStation.cs
+++ Tranzpord/Assets/Code/Stations/CityBusStation.cs
@@ -2,7 +2,31 @@
 
 public class CityBusStation : MonoBehaviour {
 
+    public float Range { get; set; }
+    public FloatReference DefaultRange;
+
+    public int CitizenCoverage { get; set; }
+
+    private void Start()
+    {
+        Range = DefaultRange;
+        CalculateCoverageResidents();
+    }
+
     //Params: Coverage range, max clients, 
     //Houses nearby
     //List of all the parts installed
+
+    void CalculateCoverageResidents()
+    {
+        CitizenCoverage = 0;
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(this.transform.position, Range);
+    }
 }
