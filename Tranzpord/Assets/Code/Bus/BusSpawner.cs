2060a32e1745b70bc06222bdee19f141ddd380a0
Max Okorokov <maxim.okorokov@gmail.com>
2017-12-22 00:28:51 +0200

    Bus movement Tryouts
    cant move :(

---
+++ Tranzpord/Assets/Code/Bus/BusSpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BusSpawner : MonoBehaviour {
+
+    public GameObject Bus;
+    public CityRoute ActiveRoute;
+
+    public void SpawnBus()
+    {
+        var newBas = Instantiate(Bus);
+        newBas.GetComponent<Movement>().SetRoute(ActiveRoute);
+    }
+}
---
+++ Tranzpord/Assets/Code/Bus/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public FloatReference MoveSpeed;
+
+    CityRoute myRoute;
+
+    int nextTileIndex;
+    Vector3 targetTile;
+    bool shouldMove = false;
+
+    private void Update()
+    {
+        if (shouldMove == false)
+            return;
+
+        if (transform.position != targetTile)
+        {
+            //transform.Translate(targetTile * Time.deltaTime * MoveSpeed);
+        } else
+        SetNextTile();
+    }
+
+    public void SetRoute(CityRoute route)
+    {
+        myRoute = route;
+        nextTileIndex = 0;
+        transform.position = myRoute.RouteTiles[nextTileIndex];
+
+        SetNextTile();
+    }
+
+    void MoveToNextTile()
+    {
+
+    }
+
+    void SetNextTile()
+    {
+        if (nextTileIndex < myRoute.RouteTiles.Count)
+        {
+            nextTileIndex++;
+        }
+        else
+        {
+            nextTileIndex = 0; //should move backward
+        }
+        
+        targetTile = myRoute.RouteTiles[nextTileIndex];
+        shouldMove = true;
+    }
+
+    //Check for Station to Stop
+
+    //Rotate
+
+}
+
