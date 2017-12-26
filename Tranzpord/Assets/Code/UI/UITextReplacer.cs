6ba8770e306806b87c9880b57d78384bcb4f816f
Max Okorokov <maxim.okorokov@gmail.com>
2017-12-26 15:37:47 +0200

    GlobalMap Window added

---
+++ Tranzpord/Assets/Code/UI/UITextReplacer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextReplacer : MonoBehaviour {
+
+    public IntReference Data;
+    public Text TextField;
+
+	
+	void Start () {
+        UpdateText();
+	}
+
+    public void UpdateText()
+    {
+        TextField.text = Data.Value.ToString();
+    }
+}
