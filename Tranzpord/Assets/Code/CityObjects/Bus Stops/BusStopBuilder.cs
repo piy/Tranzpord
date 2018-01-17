ab6a3cc20b9e108301ae7d70b8ec85d243643293
Max Okorokov <maxim.okorokov@gmail.com>
2018-01-17 23:59:57 +0200

    clicks determination trouble

--- Tranzpord/Assets/Code/CityObjects/Bus Stops/BusStopBuilder.cs
+++ Tranzpord/Assets/Code/CityObjects/Bus Stops/BusStopBuilder.cs
@@ -8,20 +8,28 @@ public class BusStopBuilder : MonoBehaviour {
     public OwnedBusStops CityBusStopps;
     public Vector3Variable ClickCoordinate;
 
+    public Vector3Int placeToBuild = new Vector3Int();
 
     Grid m_Grid;
     Tilemap CityTileMap;
     TileBase Roads;
 
-    private void Start()
+    private void OnEnable()
     {
         m_Grid = Game.ActiveCity.CityClass.m_Grid;
         CityTileMap = Game.ActiveCity.CityClass.CityTileMap;
-        Roads = Game.ActiveCity.CityClass.Roads;
+        Roads = Game.ActiveCity.CityClass.Roads;        
+    }
 
+    private void Start()
+    {
         Debug.Log("I have: " + CityBusStopps.MyBusStopps.Count + " BusStops");
         BuildAllBusStopps();
     }
+
+    //edit bus Stop
+    //if tap on road = change placement
+    //esle -> (tap on build button) = build Station
 
     public void ShowBuildBusStopPanel()
     {
@@ -30,10 +38,11 @@ public class BusStopBuilder : MonoBehaviour {
             return;
         }
 
-        Vector3Int pos = m_Grid.WorldToCell(ClickCoordinate.Value);
 
-        if (CityTileMap.GetTile(pos) == Roads)
+        if (CityTileMap.GetTile(m_Grid.WorldToCell(ClickCoordinate.Value)) == Roads)
         {
+            placeToBuild = m_Grid.WorldToCell(ClickCoordinate.Value);
+            //show transperant model and some data (radius and coverage)
             BusStopsEditWindow.Instance.ShowBuildBusStopPanel();
         }
     }
@@ -41,13 +50,10 @@ public class BusStopBuilder : MonoBehaviour {
 
     public void BuildBusStop()
     {
-        Vector3Int pos = m_Grid.WorldToCell(ClickCoordinate.Value);
+        var newBusStopInstance = CreateBusStop(placeToBuild);
+        var newBusStopData = newBusStopInstance.GetComponent<CityBusStop>().myData;
 
-        var newBusStopInstance = CreateBusStop(pos);
-        var newBusStop = newBusStopInstance.GetComponent<CityBusStop>();
-        newBusStop.myData.Placement = pos;
-
-        CityBusStopps.AddBusStop(newBusStop.myData);
+        CityBusStopps.AddBusStop(newBusStopData);
         BusStopsEditWindow.Instance.HideBuildBusStopPanel();
     }
 
@@ -55,7 +61,6 @@ public class BusStopBuilder : MonoBehaviour {
     {
         var newBusStopInstance = Instantiate(BusStop);
         var newBusStop = newBusStopInstance.GetComponent<CityBusStop>();
-
         newBusStop.CreateBusStation(place);
 
         return newBusStopInstance;
--- Tranzpord/Assets/Code/Input/Click.cs
+++ Tranzpord/Assets/Code/Input/Click.cs
@@ -6,11 +6,38 @@ public class Click : MonoBehaviour {
     public Vector3Variable ClickCoordinates;
     public GameEvent ClickEvent;
 
-	void Update () {
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             ClickCoordinates.SetValue(Camera.main.ScreenToWorldPoint(Input.mousePosition));
             ClickEvent.Raise();
         }
-	}
+    }
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        //var ray = cam.ScreenToWorldPoint(Input.mousePosition);
+    //        //RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.zero);
+
+    //        var focus = EventSystem.current.;
+
+    //        if (focus)
+    //        {
+    //            Debug.Log(focus);
+    //        }
+
+    //        ClickCoordinates.SetValue(cam.ScreenToWorldPoint(Input.mousePosition));
+    //        ClickEvent.Raise();
+    //    }
+    //}
 }
